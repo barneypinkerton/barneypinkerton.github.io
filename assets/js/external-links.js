bc72f018172f90e5a98eb11d1196119ead9ba849
barneypinkerton <barneyjpinkerton@gmail.com>
2025-11-28 12:09:27 -0500

    Update external-links.js

--- assets/js/external-links.js
+++ assets/js/external-links.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Select all external links
+  const links = document.querySelectorAll('a[href^="http"]:not([href*="' + window.location.hostname + '"])');
 
+  links.forEach(link => {
+    link.setAttribute("target", "_blank");
+    link.setAttribute("rel", "noopener noreferrer");
+  });
+});
